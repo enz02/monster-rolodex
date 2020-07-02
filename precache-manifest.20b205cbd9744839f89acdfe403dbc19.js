862f2ab92086276cbe5d5002e93bf980cbaa61c2
enz02 <enzogamilla@gmail.com>
2020-07-02 13:22:55 +0400

    Updates

---
+++ precache-manifest.20b205cbd9744839f89acdfe403dbc19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f066f00ea89a4d5e2dbe53905bab032",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "64562d2a32d101b795a9",
+    "url": "/monster-rolodex/static/css/main.fa24a98c.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64562d2a32d101b795a9",
+    "url": "/monster-rolodex/static/js/main.927bb7ea.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  }
+]);
